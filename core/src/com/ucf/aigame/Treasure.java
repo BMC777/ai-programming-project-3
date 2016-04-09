bbbfdd23078cd89a135f34fb1bbc457773d0e42d
steven <sparra95@gmail.com>
2016-04-09 18:51:41 -0400

    Adding the rest of the files, nothing new from last commit

---
+++ core/src/com/ucf/aigame/Treasure.java
@@ -0,0 +1,19 @@
+package com.ucf.aigame;
+
+import com.badlogic.gdx.math.Vector2;
+
+/**
+ * Created by Steven on 4/6/2016.
+ */
+public class Treasure {
+
+    private float value;
+    private float weight;
+
+    private Vector2 position;
+    private Vector2 center;
+
+    Treasure() {
+
+    }
+}
